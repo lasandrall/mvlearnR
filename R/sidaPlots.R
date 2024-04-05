0bd727e4f63697e9742d721f72f542076a357031
Jeremy Chacon <chaco001@umn.edu>
2024-04-05 16:38:05 -0500

    correlationPlots return ggplot or list of ggplots

--- R/sidaPlots.R
+++ R/sidaPlots.R
@@ -63,84 +63,69 @@ CorrelationPlots=function(Xtestdata=Xtestdata,Ytest=Ytest,hatalpha=hatalpha,meth
   if(is.null(color.palette)){
     color.palette=color.BlueOrange(length(unique(Ytest)))
   }
+  # determine number of correlations to make
   dsizes=lapply(Xtestdata, function(x) dim(x))
   D=length(dsizes)
   mycomb=t(utils::combn(D, 2, FUN = NULL, simplify = TRUE))
 
-  # nc=unique(Ytest)
-  # nnc=length(unique(Ytest))
-  # mycol=mat.or.vec(length(Ytest),1)
-  # mypch=mat.or.vec(length(Ytest),1)
-  # for(j in 1:length(mycol)){
-  #   mycol[j]=nc[Ytest[j]]
-  #   mypch[j]=nc[Ytest[j]]
-  # }
-
-  for(d in 1:dim(mycomb)[1]){
-    dd=mycomb[d,]
-    Scoresd=scale(Xtestdata[[dd[1]]])%*%hatalpha[[dd[1]]]
-    Scoresj=scale(Xtestdata[[dd[2]]])%*%hatalpha[[dd[2]]]
-    Scores=cbind.data.frame(Scoresd[,1], Scoresj[,1], Ytest)
-    colnames(Scores)=c("Disc1", "Disc2", "Class")
-
-    #calculate RV coefficient
-    X1=scale(Xtestdata[[dd[1]]])%*%hatalpha[[dd[1]]]
-    X2=scale(Xtestdata[[dd[2]]])%*%hatalpha[[dd[2]]]
-    X1=scale(X1, center=TRUE,scale=FALSE)
-    X2=scale(X2, center=TRUE,scale=FALSE)
-    X1X2=t(X1)%*%X2/dim(X1)[1]
-    X1X1=t(X1)%*%X1/dim(X1)[1]
-    X2X2=t(X2)%*%X2/dim(X2)[1]
-    RVCoeff=sum(diag(X1X2%*%t(X1X2)))/(sqrt(sum(diag(X1X1%*%X1X1)))*sqrt(sum(diag(X2X2%*%X2X2))))
-    RVCoeff=round(RVCoeff,digits=2)
-
-
-    # plot(Scores[,1], Scores[,2],col=mycol,lwd=3,pch=mypch
-    #      ,xlab=paste(
-    #        "First Discriminant Score for View", dd[1]),ylab=paste("First Discriminant Score for View", dd[2]),xaxt="n",
-    #      yaxt="n", main=paste("Correlation plot for views",dd[1], "and" ,dd[2],  "\u03C1 =", RVCoeff))
-    # graphics::legend("topleft",bty = "n",cex=0.8,legend=c(nc) ,col = 1:max(nc), pch=1:max(nc),title="Class")
-
-    Classes=factor(Ytest)
-    print(
-    ggplot2::ggplot(Scores, aes(Scores[,1], Scores[,2])) +
-      geom_point(aes(shape=Classes,colour = Classes),size=4) +
-      xlab(
-        if(method.used=="SIDA"){
-          paste(
-            "First Discriminant Score for View ", dd[1])
-        }else if(method.used=="SELPCCA"){
-          paste("First Canonical Variate for View ",dd[1])
-        }
-      )+
-      ylab(
-        if(method.used=="SIDA"){
-          paste(
-            "First Discriminant Score for View ", dd[2])
-        }else if(method.used=="SELPCCA"){
-          paste("First Canonical Variate for View ",dd[2])
-        }
-      )+
-      # xlab(paste(
-      #   "First Discriminant Score for View", dd[1])) +
-      # ylab(paste("First Discriminant Score for View", dd[2])) +
-      ggtitle(paste("Correlation plot for views",dd[1], "and" ,dd[2],",", "\u03C1 =", RVCoeff)) +
-      scale_colour_manual(values=color.palette) +
-      scale_fill_manual(values = color.palette) +
-      theme_bw() +
-      theme(axis.title = element_text(face="bold"))+
-      theme(axis.text = element_blank())+
-      theme(axis.ticks.x = element_blank())+
-      theme(axis.ticks.y=element_blank())+
-      theme(axis.ticks = element_blank())
-    )
-
-
-
+  Classes = factor(Ytest)
+  # make ggplots for each plot and add them to a list
+  plots = lapply(1:dim(mycomb)[1],
+                 FUN = function(d){
+                   dd=mycomb[d,]
+                   Scoresd=scale(Xtestdata[[dd[1]]])%*%hatalpha[[dd[1]]]
+                   Scoresj=scale(Xtestdata[[dd[2]]])%*%hatalpha[[dd[2]]]
+                   Scores=cbind.data.frame(Scoresd[,1], Scoresj[,1], Ytest)
+                   colnames(Scores)=c("Disc1", "Disc2", "Class")
+                   
+                   #calculate RV coefficient
+                   X1=scale(Xtestdata[[dd[1]]])%*%hatalpha[[dd[1]]]
+                   X2=scale(Xtestdata[[dd[2]]])%*%hatalpha[[dd[2]]]
+                   X1=scale(X1, center=TRUE,scale=FALSE)
+                   X2=scale(X2, center=TRUE,scale=FALSE)
+                   X1X2=t(X1)%*%X2/dim(X1)[1]
+                   X1X1=t(X1)%*%X1/dim(X1)[1]
+                   X2X2=t(X2)%*%X2/dim(X2)[1]
+                   RVCoeff=sum(diag(X1X2%*%t(X1X2)))/(sqrt(sum(diag(X1X1%*%X1X1)))*sqrt(sum(diag(X2X2%*%X2X2))))
+                   RVCoeff=round(RVCoeff,digits=2)
+                   ggplot2::ggplot(Scores, aes(Scores[,1], Scores[,2])) +
+                     geom_point(aes(shape=Classes,colour = Classes),size=4) +
+                     xlab(
+                       if(method.used=="SIDA"){
+                         paste(
+                           "First Discriminant Score for View ", dd[1])
+                       }else if(method.used=="SELPCCA"){
+                         paste("First Canonical Variate for View ",dd[1])
+                       }
+                     )+
+                     ylab(
+                       if(method.used=="SIDA"){
+                         paste(
+                           "First Discriminant Score for View ", dd[2])
+                       }else if(method.used=="SELPCCA"){
+                         paste("First Canonical Variate for View ",dd[2])
+                       }
+                     )+
+                     ggtitle(paste("Correlation plot for views",dd[1], "and" ,dd[2],",", "\u03C1 =", RVCoeff)) +
+                     scale_colour_manual(values=color.palette) +
+                     scale_fill_manual(values = color.palette) +
+                     theme_bw() +
+                     theme(axis.title = element_text(face="bold"))+
+                     theme(axis.text = element_blank())+
+                     theme(axis.ticks.x = element_blank())+
+                     theme(axis.ticks.y=element_blank())+
+                     theme(axis.ticks = element_blank())
+                 })
+  # return the actual ggplot object if there was only one; otherwise,
+  # return a list of ggplot objects, and a message to print using grid.arrange()
+  if (length(plots) == 1){
+    return(plots[[1]])
   }
-  return(invisible(NULL))
-
-
+  message(">1 data views, returning list of ggplot objects.
+          show the output of this function with:
+              plots = CorrelationPlots(...)
+              gridExtra::grid.arrange(grobs = plots)")
+  return(plots)
 }
 
 
