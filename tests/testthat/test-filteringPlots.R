85f03721fcbb041efa8eb33e7e9537683e78a638
Jeremy Chacon <chaco001@umn.edu>
2024-05-02 10:17:08 -0500

    added tests for filteringPlots

---
+++ tests/testthat/test-filteringPlots.R
@@ -0,0 +1,21 @@
+test_that("umapPlot returns a list of two ggplots on sida filtered ", {
+  expect_equal({
+    load("../../data/sida_and_sidanet_vignette_models.Rdata")
+    sida_filtered = filterOmics
+    sida_fit = fit.cvsida
+    x = umapPlot(sida_filtered, plotIt = FALSE)
+    c(class(x), length(x), class(x[[1]]))
+  }, 
+  c("list", "2", "gg", "ggplot"))
+})
+
+test_that("volcanoPlot returns a list of two ggplots on sida filtered ", {
+  expect_equal({
+    load("../../data/sida_and_sidanet_vignette_models.Rdata")
+    sida_filtered = filterOmics
+    sida_fit = fit.cvsida
+    x = volcanoPlot(sida_filtered, plotIt = FALSE)
+    c(class(x), length(x), class(x[[1]]))
+  }, 
+  c("list", "2", "gg", "ggplot"))
+})
